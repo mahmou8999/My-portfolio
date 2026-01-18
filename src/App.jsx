64cccb5df050bc7f05ee7cf6608c10a966d955e2
mahmoud8999 <mahmoud.rabie2512@gmail.com>
2026-01-18 19:15:49 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Projects from "./components/projects";
